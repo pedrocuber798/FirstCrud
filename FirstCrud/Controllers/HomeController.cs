f4d17b99d3a63d9eafb52fc96e4c559e39f24f66
Pedro Cuber Pinto <pedro.pinto@al.infnet.edu.br>
2022-08-23 08:03:08 -0300

    ?

--- FirstCrud/Controllers/HomeController.cs
+++ FirstCrud/Controllers/HomeController.cs
@@ -13,6 +13,14 @@ namespace FirstCrud.Controllers {
         public IActionResult Index() {
             return View();
         }
+
+        public IActionResult Index1() {
+            return View();
+        }
+
+        public IActionResult Index2() {
+            return View();
+        }
         public IActionResult Privacy() {
             return View();
         }
